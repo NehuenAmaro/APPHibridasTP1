651ab0c03dc8d5be30c5ebd2fd2c28e6473b4bf8
Nehuen <110565087+NehuenAmaro@users.noreply.github.com>
2023-12-05 23:04:59 -0300

    Delete TP1  directory

--- TP1 /routes/gameVote.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import GameVotesController from '../controllers/gameVote.js'
-
-const route = express.Router()
-
-/* route.get('/votes', GameVotesController.getVotes) */
-route.post('/votes', GameVotesController.createVote)
-
-export default route
